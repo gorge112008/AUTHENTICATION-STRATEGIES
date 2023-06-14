23ad45a610fdc49a1c828731b1a985cb5887b5c3
Jorge Carlos Gayoso Ugas <gorge112008@hotmail.com>
2023-06-14 00:02:49 -0500

    Autenticacion Encriptada Exitosa

--- src/utils.js
+++ src/utils.js
@@ -3,7 +3,7 @@ import { dirname } from 'path';
 import brcrypt from 'bcrypt';
 
 export const createHash = password => brcrypt.hashSync(password, brcrypt.genSaltSync(10));
-export const isValidPassword = (user, password) => brcrypt.compareSync(password, user.password);
+export const isValidPassword = (password, user) => brcrypt.compareSync(password, user);
 
 const __filename = fileURLToPath(import.meta.url); //Obtener la ruta absoluta del directorio actual
 const __dirname = dirname(__filename);
